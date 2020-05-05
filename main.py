deeadf6341039923dac97f16ba74095f195e87a0
Young <wook2124@naver.com>
2020-05-05 06:50:11 +0000

    #4.1 Introduction to Flask

--- main.py
+++ main.py
@@ -2,4 +2,12 @@ from flask import Flask
 
 app = Flask("SuperScrapper")
 
+@app.route("/")
+def home():
+  return "Hello! Welcome to mi casa!"
+
+@app.route("/contact")
+def blahblah():
+  return "Contact me!"
+
 app.run(host="0.0.0.0")

ce85a29505ca86feed6eeffd3e5f00a8e7318a92
Young <wook2124@naver.com>
2020-05-05 07:05:19 +0000

    #4.2 Dynamic URLs and Templates

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask("SuperScrapper")
 
+
 @app.route("/")
 def home():
-  return "Hello! Welcome to mi casa!"
+    return render_template("home.html")
 
-@app.route("/contact")
-def blahblah():
-  return "Contact me!"
+
+@app.route("/<username>")
+def blahblah(username):
+    return f"Hello, your name is {username}"
+
 
 app.run(host="0.0.0.0")

043c7dc8ecccef6b0a30c24af1dcbf5446ce907a
Young <wook2124@naver.com>
2020-05-08 05:06:59 +0000

    #4.4 Scrapper Integration

--- Lecture/#4.4 Scrapper Integration.py
+++ Lecture/#4.4 Scrapper Integration.py
@@ -110,3 +110,50 @@ def get_jobs(word):
   last_page = get_last_page(url)
   jobs = extract_jobs(last_page, url)
   return jobs
+
+
+# stackoverflow scrapper로 바꿈
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_last_page(url):
+    result = requests.get(url)
+    soup = BeautifulSoup(result.text, "html.parser")
+    pages = soup.find("div", {"class": "s-pagination"}).find_all("a")
+    last_page = pages[-2].get_text(strip=True)
+    return int(last_page)
+
+
+def extract_job(html):
+    title = html.find("h2").find("a")["title"]
+    company, location = html.find("h3").find_all("span", recursive=False)
+    company = company.get_text(strip=True)
+    location = location.get_text(strip=True).strip("-").strip("\r").strip("\n")
+    job_id = html['data-jobid']
+    return {
+        "title": title,
+        "company": company,
+        "location": location,
+        "link": f"https://stackoverflow.com/jobs/{job_id}"
+    }
+
+
+def extract_jobs(last_page, url):
+    jobs = []
+    for page in range(last_page):
+        print(f"Scrapping Page: {page}")
+        result = requests.get(f"{url}&pg={page + 1}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all("div", {"class": "-job"})
+        for result in results:
+            job = extract_job(result)
+            jobs.append(job)
+    return jobs
+
+
+def get_jobs(word):
+    url = f"https://stackoverflow.com/jobs?q={word}&sort=i"
+    last_page = get_last_page(url)
+    jobs = extract_jobs(last_page, url)
+    return jobs
